69edb75a98086101a9883b4db7aec04b8d831ed7
Your Name <youremail@domain.com>
2022-02-10 21:10:27 +0530

    splash screen added

---
+++ lib/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'getting_started.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (Context) => const GettingStarted()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/food.png'),
+            const SizedBox(height: 8.0),
+            const Text(
+              "No waiting for food",
+              style: TextStyle(
+                fontSize: 16,
+                color: Color(0xff838383),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
