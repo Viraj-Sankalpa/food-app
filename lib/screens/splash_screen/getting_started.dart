93d3c0e19d3dc7a964e90eaad1b2121b36ea266c
Your Name <youremail@domain.com>
2022-04-09 00:35:29 +0530

    finish getting started

--- lib/screens/splash_screen/getting_started.dart
+++ lib/screens/splash_screen/getting_started.dart
@@ -1,10 +1,37 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/utils/app_colors.dart';
 import 'package:food_app/utils/constants.dart';
 
-class GettingStarted extends StatelessWidget {
+class GettingStarted extends StatefulWidget {
   const GettingStarted({Key? key}) : super(key: key);
 
+  @override
+  State<GettingStarted> createState() => _GettingStartedState();
+}
+
+class _GettingStartedState extends State<GettingStarted> {
+  CarouselController carouselController = CarouselController();
+  List<Widget> list = [
+    SliderItems(
+      img: 'popcorn.png',
+      text1: 'Choose A Tasty Dish',
+      text2: 'Order anythinng you want from your\n Favourite restaurent.',
+    ),
+    SliderItems(
+      img: 'money.png',
+      text1: 'Easy Payment',
+      text2:
+          'Payment made easy through debit\n card, credit card  & more ways to pay\n for your food.',
+    ),
+    SliderItems(
+      img: 'restaurant.png',
+      text1: 'Enjoy the Taste!',
+      text2:
+          'Healthy eating means eating a variety of foods that give you the nutrients you need to maintain your health.',
+    ),
+  ];
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -15,49 +42,156 @@ class GettingStarted extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              child: Stack(
-                children: [
-                  Image.asset(
-                    Constants.imageAssest('bottom.png'),
-                    width: size.width,
-                    fit: BoxFit.fitWidth,
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: size.height / 2,
+                  // autoPlay: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
+              carouselController: carouselController,
+              items: list.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      child: Column(
+                        children: [i],
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: list.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: 12.0,
+                    height: 12.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white
+                                : Colors.black)
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
                   ),
-                  Positioned(
-                    bottom: 39,
-                    left: 182,
-                    child: Row(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Next',
-                            style: TextStyle(color: kblack),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: kwhite,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Skip',
-                            style: TextStyle(
-                              color: kblack,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                );
+              }).toList(),
+            ),
+            BottomSection(
+              size: size,
+              ontap: () => carouselController.nextPage(),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SliderItems extends StatelessWidget {
+  const SliderItems({
+    Key? key,
+    required this.img,
+    required this.text1,
+    required this.text2,
+  }) : super(key: key);
+
+  final String img;
+  final String text1;
+  final String text2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          Constants.imageAssest(img),
+        ),
+        SizedBox(
+          height: 37,
+        ),
+        Text(
+          text1,
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(
+          text2,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14,
+            color: grayColor,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BottomSection extends StatelessWidget {
+  const BottomSection({
+    Key? key,
+    required this.size,
+    required this.ontap,
+  }) : super(key: key);
+
+  final Size size;
+  final Function() ontap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Image.asset(
+            Constants.imageAssest('bottom.png'),
+            width: size.width,
+            fit: BoxFit.fitWidth,
+          ),
+          Positioned(
+            bottom: 39,
+            left: 270,
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    ontap();
+                  },
+                  child: Text(
+                    'Next',
+                    style: TextStyle(color: kblack),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: kwhite,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: kblack,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
